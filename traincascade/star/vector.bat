3b1eab6cc64909254659d3e4c6ef6bee02737274
Murat <83974970+Dobreli@users.noreply.github.com>
2021-11-06 15:55:03 +0300

    Rename vektor.bat to vector.bat

---
+++ traincascade/star/vector.bat
@@ -0,0 +1,1 @@
+C:\Users\DobreLi\Desktop\opencv\build\x64\vc15\bin\opencv_createsamples.exe -info positive.txt -num 1024 -vec el.vec -w 24 -h 24
--- traincascade/star/vektor.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\DobreLi\Desktop\opencv\build\x64\vc15\bin\opencv_createsamples.exe -info positive.txt -num 1024 -vec el.vec -w 24 -h 24
